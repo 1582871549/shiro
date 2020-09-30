772cfdabb8831ea494c6045550df8eb4b7c79e80
dujianwei <1582871549@qq.com>
2020-09-30 18:05:26 +0800

    基于token重新拟定身份校验流程

--- src/main/java/com/meng/user/shiro/filter/CustomAuthcFilter.java
+++ src/main/java/com/meng/user/shiro/filter/CustomAuthcFilter.java
@@ -4,6 +4,7 @@ import com.meng.user.common.util.JwtHelper;
 import com.meng.user.repository.entity.UserDO;
 import com.meng.user.service.system.UserService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
@@ -12,55 +13,82 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 public class CustomAuthcFilter extends FormAuthenticationFilter {
+
+    private static final String TOKEN = "token";
 
     @Autowired
     private UserService userService;
 
     @Override
-    protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        return false;
+    }
 
-        String token = getToken(request);
-
-        // token为null, 允许访问登录接口使用账号密码登录
-        if (token == null) {
-            return true;
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        // 是否携带token
+        if (isCarryToken(request)) {
+            return verifyToken(request, response);
         } else {
-            return verify(token, request, response);
+            if (isLoginRequest(request, response)) {
+                // 是登录请求, 放过, 允许请求登录接口
+                return true;
+            } else {
+                // 非登录请求, 重定向到登录页面
+                saveRequestAndRedirectToLogin(request, response);
+                return false;
+            }
         }
     }
 
-    private String getToken(ServletRequest servletRequest) {
-
-        HttpServletRequest request = WebUtils.toHttp(servletRequest);
-
-        return request.getHeader("token");
+    /**
+     * 用户是否携带token
+     *
+     * @return <code>true</code> 携带
+     */
+    private boolean isCarryToken(ServletRequest request) {
+        String token = getToken(request);
+        return StringUtils.isNotBlank(token);
     }
 
-    private boolean verify(String token, ServletRequest request, ServletResponse response) throws Exception {
-
-        Subject subject = getSubject(request, response);
-        PrincipalCollection principals = subject.getPrincipals();
-
-        if (principals == null || principals.isEmpty()) {
-            return true;
-        }
-
-        String username = String.valueOf(principals.getPrimaryPrincipal());
-
-        if (StringUtils.isBlank(username)) {
-            return true;
-        }
-
-        UserDO userDO = userService.getUserByUsername(username);
+    private boolean verifyToken(ServletRequest request, ServletResponse response) throws Exception {
+        String username = getPrincipal();
+        UserDO userDO = getUser(username);
+        String token = getToken(request);
 
         if (userDO != null && JwtHelper.verify(token, userDO.getPassword())) {
             return onLoginSuccess(request, response);
         } else {
             return onLoginFailure(request, response);
         }
+    }
+
+    private String getToken(ServletRequest request) {
+        return WebUtils.toHttp(request).getHeader(TOKEN);
+    }
+
+    private String getPrincipal() {
+        Subject subject = getSubject();
+        PrincipalCollection principals = subject.getPrincipals();
+
+        if (principals == null || principals.isEmpty()) {
+            return "";
+        }
+        return String.valueOf(principals.getPrimaryPrincipal());
+    }
+
+    private Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+
+    private UserDO getUser(String username) {
+        if (StringUtils.isBlank(username)) {
+            return null;
+        }
+        return userService.getUserByUsername(username);
     }
 
     /**
@@ -74,10 +102,9 @@ public class CustomAuthcFilter extends FormAuthenticationFilter {
     /**
      * 登录失败，让请求继续返回登录页面:
      */
-    private boolean onLoginFailure(ServletRequest request, ServletResponse response) {
-        return true;
+    private boolean onLoginFailure(ServletRequest request, ServletResponse response) throws IOException {
+        redirectToLogin(request, response);
+        return false;
     }
-
-
 
 }
